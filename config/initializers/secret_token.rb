7f6e9f2eb26025a6f356bca6e1c68a62f529a642
App Academy Student <student@appacademy.io>
2014-05-02 10:34:35 -0700

    Initial commit, set up db

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LiftClone::Application.config.secret_key_base = '4e61a17e137e5c5781610c5130a72de8a3c48de2c711f69154b9ed6c5f6f4d157bab6d24f8bedf9343743bc85091ed8e6bbcbf4ae560e433296b2bd53b1ea1a8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LiftClone::Application.config.session_store :cookie_store, key: '_lift_clone_session'
